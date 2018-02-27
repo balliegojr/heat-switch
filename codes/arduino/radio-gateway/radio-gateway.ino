1e80448e813956dc755ab39d4fba0fe242af6be0
Ilson Roberto Balliego Junior <ilson.balliego@outlook.com>
2018-02-27 11:23:54 -0300

    Adding arduino sketches

---
+++ codes/arduino/radio-gateway/radio-gateway.ino
@@ -0,0 +1,51 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const int temperatureSensorPin = 0; 
+const int powerRelayPin = 10;
+
+const float upperCelciusLimit = 32.0;
+const float lowerCelciusLimit = 28.0;
+
+int powerRelayStatus = LOW;
+
+RF24 radio(7, 8); // CE, CSN
+const uint64_t r_pipe = 0x0606060606;
+const uint64_t w_pipe = 0x0707070707;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  radio.begin();
+  radio.openReadingPipe(0, r_pipe);
+  radio.openWritingPipe(w_pipe);
+  radio.setChannel(0x60);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+  
+  Serial.println("Board available");
+}
+ 
+void loop()
+{
+ if (radio.available()){
+  char radio_info[32];
+  radio.read(&radio_info, 32);
+
+  Serial.println(radio_info);
+ }
+
+ if (Serial.available()){
+  Serial.println("Receiving serial info");
+  
+  char serial_info[32];
+  Serial.readBytes(serial_info, 32);
+  radio.stopListening();
+  radio.write(&serial_info, 32);
+  radio.startListening();
+ }
+
+ delay(20);
+}
---
+++ codes/arduino/temperature-power-switch/temperature-power-switch.ino
@@ -0,0 +1,244 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include <EEPROM.h>
+
+const int uid_address = 0;
+const uint64_t w_pipe = 0x0606060606;
+const uint64_t r_pipe = 0x0707070707;
+
+char uid[] = "0000000";
+char op_mode = 'A';
+
+const int temperatureSensorPin = 0; 
+const int powerRelayPin = 10;
+
+float upperCelciusLimit = 32.0;
+float lowerCelciusLimit = 28.0;
+
+int powerRelayStatus = LOW;
+int override_relay = LOW;
+int current_loop = 0;
+
+RF24 radio(7, 8); // CE, CSN
+
+void setup()
+{
+  Serial.begin(9600);
+
+  analogReference(INTERNAL);
+
+  pinMode(powerRelayPin, OUTPUT);
+  digitalWrite(powerRelayPin, powerRelayStatus);
+
+  if (EEPROM.read(0) == 'C')
+    for (int i = 0; i<7; i++) {
+      uid[i] = EEPROM.read(i);
+    }
+  
+
+  radio.begin();
+  radio.openWritingPipe(w_pipe);
+  radio.openReadingPipe(0, r_pipe);
+  radio.setChannel(0x60);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+
+void send_radio(char text[]) {
+  if (strcmp(uid, "0000000") == 0){
+    return;
+  }
+
+  
+  Serial.println("Sending radio message");
+
+  char package[32];
+  strcpy(package, uid);
+  strcat(package, ":");
+  strcat(package, text);
+
+  radio.stopListening();
+  radio.write(package, 32);
+  radio.startListening();
+}
+
+void requestId() {
+  Serial.println("Requesting Id");
+  radio.stopListening();
+  char request[] = "I";
+
+  radio.write(&request, 1);
+
+  radio.startListening();
+  
+}
+
+void set_info(char* info) {
+  info++;
+  
+  switch (info[0]) {
+    case 'I':
+        for (int i=0; i<7; i++ ) {
+            uid[i] = info[i+1];
+        }
+
+        EEPROM.put(uid_address, uid);
+        break;
+    case 'M':
+        info++;
+        op_mode = info[0];
+        switch (op_mode) {
+            case 'A': {
+                info += 2;
+
+                char str_upper[] = "000.0";
+                char str_lower[] = "000.0";
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    str_upper[i] = info[i];
+                    str_lower[i] = info[i+6];
+                }
+
+                upperCelciusLimit = atof(str_upper);
+                lowerCelciusLimit = atof(str_lower);
+                
+                break;
+            }
+            case 'O':
+                info++;
+                //set override:
+                if (info[0] == '1') {
+                  override_relay = HIGH;
+                } else {
+                  override_relay = LOW;
+                }
+
+                break;
+            case 'R':
+                override_relay = LOW;
+                override_mode();
+                break;
+            }
+
+        break;
+  }
+}
+
+void override_mode() {
+  if (override_relay != powerRelayStatus){
+    powerRelayStatus = override_relay;
+    digitalWrite(powerRelayPin, powerRelayStatus);
+    char status_str[4];
+    if (powerRelayStatus == HIGH) {
+      strcpy(status_str, "r:on");
+      send_radio(status_str);
+    } else {
+      strcpy(status_str, "r:off");
+      send_radio(status_str);
+    }
+  }
+}
+
+void report_mode() {
+  if (current_loop % 20 != 0){
+    return;
+  }
+  
+  int temperatureC = read_temp();
+
+  char text[32];
+  sprintf(text, "t:%d", temperatureC);
+  send_radio(text);
+}
+
+int read_temp() {
+  int reading = analogRead(temperatureSensorPin);
+  return reading / 9.31; // using 1.1v reference
+  // return (5.0 * reading * 100.0) / 1024;
+  
+}
+
+void automatic_mode() {
+  if (current_loop % 20 != 0){
+    return;
+  }
+  
+  int temperatureC = read_temp();
+ 
+  int newRelayStatus = powerRelayStatus;
+  if (temperatureC >= upperCelciusLimit){
+    newRelayStatus = LOW;
+  } else if (temperatureC <= lowerCelciusLimit){
+    newRelayStatus = HIGH; 
+  }
+
+  if (newRelayStatus != powerRelayStatus){
+    powerRelayStatus = newRelayStatus;
+    digitalWrite(powerRelayPin, powerRelayStatus);
+    char status_str[4];
+    if (powerRelayStatus == HIGH) {
+      strcpy(status_str, "r:on");
+      send_radio(status_str);
+    } else {
+      strcpy(status_str, "r:off");
+      send_radio(status_str);
+    }
+ } 
+
+ char text[32];
+ sprintf(text, "t:%d", temperatureC);
+ send_radio(text);
+}
+
+void process_radio_message(char* message) {
+    for (int i=0; i < 7; i++){
+        if (uid[i] != message[i]){
+          return;
+        }
+    }
+
+    char* command = message + 8;
+
+    if (command[0] == 'S') {
+        printf("command: ");
+        printf(command);
+        printf("\n");
+        set_info(command);
+    }
+}
+ 
+void loop()
+{
+  if (strcmp(uid, "0000000") == 0) {
+    requestId();  
+  }
+
+  if (radio.available()) {
+    char radio_info[32];
+    radio.read(&radio_info, 32);
+    
+    process_radio_message(radio_info);
+  }
+  
+  switch (op_mode) {
+    case 'O':
+      override_mode();
+      break;
+    case 'A':
+      automatic_mode();
+      break;
+    case 'R':
+      report_mode();    
+      break;
+  }
+
+  current_loop++;
+  if (current_loop == 100000){
+    current_loop = 0;
+  }
+  delay(100);                                     
+}
