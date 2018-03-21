0745a4ca55840694cf90ed0ff8fc8094cae857a9
Ilson Roberto Balliego Junior <ilson.balliego@outlook.com>
2018-03-21 18:42:50 -0300

    Adding pt_BR translation

--- src/sensor_nodes/lib/sensor_nodes_web/router.ex
+++ src/sensor_nodes/lib/sensor_nodes_web/router.ex
@@ -7,6 +7,7 @@ defmodule SensorNodesWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug SensorNodesWeb.Locale
   end
 
   pipeline :auth do
